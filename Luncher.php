6dc9d2a6a0447972e0805e72861e77cbf179c389
aminyou <amin.loves.you@gmail.com>
2016-08-08 22:06:32 +0430

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '264107946:AAFk4WVCoLzghobGGu47pwA-6acfaTUVExI');
+define('BOT_TOKEN', '216121596:AAGST9M-paaAPspkf8zr7r57Q1znCFBAdaQ');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = 192050231;
+  $admin = 208895732;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
